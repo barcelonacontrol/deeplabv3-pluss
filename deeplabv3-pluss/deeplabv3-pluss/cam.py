6329c6f6995c4325580a96d4d0594c82d924d907
whf <1292597139@qq.com>
2024-09-27 00:29:29 +0800

    send

---
+++ deeplabv3-pluss/deeplabv3-pluss/cam.py
@@ -0,0 +1,266 @@
+import cv2
+import time
+import os
+import matplotlib.pyplot as plt
+import torch
+from torch import nn
+import torchvision.transforms as transforms
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from nets.xception import xception
+from nets.mobilenetv2 import mobilenetv2
+# 是否使用cuda
+savepath = r'cam_map'
+if not os.path.exists(savepath):
+    os.mkdir(savepath)
+
+def draw_features(width, height, x, savename):
+    tic = time.time()
+    fig = plt.figure(figsize=(16, 16))
+    fig.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95, wspace=0.05, hspace=0.05)
+    for i in range(width * height):
+        plt.subplot(height, width, i + 1)
+        plt.axis('off')
+        #img = x[0, i, :, :]
+        img = x[0, i, :, :]
+        pmin = np.min(img)
+        pmax = np.max(img)
+        img = ((img - pmin) / (pmax - pmin + 0.000001)) * 255  # float在[0，1]之间，转换成0-255
+        img = img.astype(np.uint8)  # 转成unit8
+        img = cv2.applyColorMap(img, cv2.COLORMAP_JET)  #生成heat map
+        img = img[:, :, ::-1]  # 注意cv2（BGR）和matplotlib(RGB)通道是相反的
+        plt.imshow(img)
+        print("{}/{}".format(i, width * height))
+    fig.savefig(savename, dpi=100)
+    fig.clf()
+    plt.close()
+    print("time:{}".format(time.time() - tic))
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(DoubleConv, self).__init__()
+        # 一层神经网络，包括卷积层、归一化、线性激活函数
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, input):
+        return self.conv(input)
+
+
+class MobileNetV2(nn.Module):
+    def __init__(self, downsample_factor=8, pretrained=True):
+        super(MobileNetV2, self).__init__()
+        from functools import partial
+
+        model = mobilenetv2(pretrained)
+        self.features = model.features[:-1]
+
+        self.total_idx = len(self.features)
+        self.down_idx = [2, 4, 7, 14]  # 根据重复次数对应到每一个下采样Block的位置
+
+        if downsample_factor == 8:  # 把步长为2的步长变为1 进行参数的修改 膨胀系数也进行修改
+            for i in range(self.down_idx[-2], self.down_idx[-1]):  # 倒数两次的步长为2的修改
+                self.features[i].apply(
+                    partial(self._nostride_dilate, dilate=2)
+                )
+            for i in range(self.down_idx[-1], self.total_idx):
+                self.features[i].apply(
+                    partial(self._nostride_dilate, dilate=4)
+                )
+        elif downsample_factor == 16:  # 对最后一个进行修改
+            for i in range(self.down_idx[-1], self.total_idx):
+                self.features[i].apply(
+                    partial(self._nostride_dilate, dilate=2)
+                )
+
+    def _nostride_dilate(self, m, dilate):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            if m.stride == (2, 2):
+                m.stride = (1, 1)
+                if m.kernel_size == (3, 3):
+                    m.dilation = (dilate // 2, dilate // 2)
+                    m.padding = (dilate // 2, dilate // 2)
+            else:
+                if m.kernel_size == (3, 3):
+                    m.dilation = (dilate, dilate)
+                    m.padding = (dilate, dilate)
+
+    def forward(self, x):
+        low_level_features = self.features[:4](x)  # 浅层的语义信息
+        x = self.features[4:](low_level_features)  # 深层的语义信息
+        return low_level_features, x
+
+    # -----------------------------------------#
+
+
+#   ASPP特征提取模块
+#   利用不同膨胀率的膨胀卷积进行特征提取
+# -----------------------------------------#
+class ASPP(nn.Module):
+    def __init__(self, dim_in, dim_out, rate=1, bn_mom=0.1):
+        super(ASPP, self).__init__()
+        self.branch1 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, 1, 1, padding=0, dilation=rate, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.branch2 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, 3, 1, padding=6 * rate, dilation=6 * rate, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.branch3 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, 3, 1, padding=12 * rate, dilation=12 * rate, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.branch4 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, 3, 1, padding=18 * rate, dilation=18 * rate, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.branch5_conv = nn.Conv2d(dim_in, dim_out, 1, 1, 0, bias=True)
+        self.branch5_bn = nn.BatchNorm2d(dim_out, momentum=bn_mom)
+        self.branch5_relu = nn.ReLU(inplace=True)
+
+        self.conv_cat = nn.Sequential(
+            nn.Conv2d(dim_out * 5, dim_out, 1, 1, padding=0, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        [b, c, row, col] = x.size()
+        # -----------------------------------------#
+        #   一共五个分支
+        # -----------------------------------------#
+        conv1x1 = self.branch1(x)
+        conv3x3_1 = self.branch2(x)
+        conv3x3_2 = self.branch3(x)
+        conv3x3_3 = self.branch4(x)
+        # -----------------------------------------#
+        #   第五个分支，全局平均池化+卷积
+        # -----------------------------------------#
+        global_feature = torch.mean(x, 2, True)
+        global_feature = torch.mean(global_feature, 3, True)
+        global_feature = self.branch5_conv(global_feature)
+        global_feature = self.branch5_bn(global_feature)
+        global_feature = self.branch5_relu(global_feature)  # 1乘1卷积 平均池化  激活函数 整合
+        global_feature = F.interpolate(global_feature, (row, col), None, 'bilinear', True)
+
+        # -----------------------------------------#
+        #   将五个分支的内容堆叠起来
+        #   然后1x1卷积整合特征。
+        # -----------------------------------------#
+        feature_cat = torch.cat([conv1x1, conv3x3_1, conv3x3_2, conv3x3_3, global_feature], dim=1)
+        result = self.conv_cat(feature_cat)
+        return result
+
+
+class DeepLab(nn.Module):
+    def __init__(self, num_classes, backbone="mobilenet", pretrained=True, downsample_factor=16):
+        super(DeepLab, self).__init__()
+        if backbone == "xception":
+            # ----------------------------------#
+            #   获得两个特征层
+            #   浅层特征    [128,128,256]
+            #   主干部分    [30,30,2048]
+            # ----------------------------------#
+            self.backbone = xception(downsample_factor=downsample_factor, pretrained=pretrained)
+            in_channels = 2048
+            low_level_channels = 256
+        elif backbone == "mobilenet":
+            # ----------------------------------#
+            #   获得两个特征层
+            #   浅层特征    [128,128,24]
+            #   主干部分    [30,30,320]
+            # ----------------------------------#
+            self.backbone = MobileNetV2(downsample_factor=downsample_factor, pretrained=pretrained)
+            in_channels = 320
+            low_level_channels = 24
+        else:
+            raise ValueError('Unsupported backbone - `{}`, Use mobilenet, xception.'.format(backbone))
+
+        # -----------------------------------------#
+        #   ASPP特征提取模块
+        #   利用不同膨胀率的膨胀卷积进行特征提取
+        # -----------------------------------------#
+        self.aspp = ASPP(dim_in=in_channels, dim_out=256, rate=16 // downsample_factor)
+
+        # ----------------------------------#
+        #   浅层特征边
+        # ----------------------------------#
+        self.shortcut_conv = nn.Sequential(
+            nn.Conv2d(low_level_channels, 48, 1),
+            nn.BatchNorm2d(48),
+            nn.ReLU(inplace=True)
+        )
+
+        self.cat_conv = nn.Sequential(
+            nn.Conv2d(48 + 256, 256, 3, stride=1, padding=1),  # 3乘以3卷积进行特征提取
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+
+            nn.Conv2d(256, 256, 3, stride=1, padding=1),  # 3乘以3卷积进行特征提取
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+
+            nn.Dropout(0.1),
+        )
+        self.cls_conv = nn.Conv2d(256, num_classes, 1, stride=1)
+
+    def forward(self, x):
+        H, W = x.size(2), x.size(3)
+        # -----------------------------------------#
+        #   获得两个特征层
+        #   low_level_features: 浅层特征-进行卷积处理
+        #   x : 主干部分-利用ASPP结构进行加强特征提取
+        # -----------------------------------------#
+        low_level_features, x = self.backbone(x)
+        x = self.aspp(x)
+        low_level_features = self.shortcut_conv(low_level_features)
+
+        # -----------------------------------------#
+        #   将加强特征边上采样
+        #   与浅层特征堆叠后利用卷积进行特征提取
+        # -----------------------------------------#
+        x = F.interpolate(x, size=(low_level_features.size(2), low_level_features.size(3)), mode='bilinear',
+                          align_corners=True)
+        x = self.cat_conv(torch.cat((x, low_level_features), dim=1))
+        draw_features(8, 8, x.cpu().numpy(), "{}/out.png".format(savepath))
+        x = self.cls_conv(x)
+        x = F.interpolate(x, size=(H, W), mode='bilinear', align_corners=True)
+        return x
+
+num_classes = 5
+backbone    = "mobilenet"
+pretrained  = False
+downsample_factor   = 16
+model_path  = "model_data/deeplab_mobilenetv2.pth"
+savepath = r'cam_map'
+model   = DeepLab(num_classes=num_classes, backbone=backbone, downsample_factor=downsample_factor, pretrained=pretrained).cuda()
+model.eval()
+img = cv2.imread("img/1.jpg")
+img = cv2.resize(img, (224, 224))
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+img = transform(img).cuda()
+img = img.unsqueeze(0)
+
+with torch.no_grad():
+    start=time.time()
+    out=model(img)
+    print("total time:{}".format(time.time()-start))
+    print("done")
